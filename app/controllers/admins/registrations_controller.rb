1698f5f6076a3e9ccf923264e2d7a1af4f444767
hiro4869 <sakuragima48@gmail.com>
2017-06-02 16:28:07 +0900

    管理ユーザーをウェブ上から登録できないように実装

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -3,9 +3,10 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    # super
+    redirect_to root_path
+  end
 
   # POST /resource
   # def create
